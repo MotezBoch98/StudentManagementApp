97018f50da6808ba7da101050b7a62810d526596
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-01-25 18:14:52 +0100

    Login page

---
+++ src/studentmangementapp/FXMLDocumentController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentmangementapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Motez
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+    
+    public void close(){
+        System.exit(0);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
