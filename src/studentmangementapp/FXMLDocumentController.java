d17ca6325fc41a72a56d5292686ccf6cd072b6a8
MotezBoch98 <89611046+MotezBoch98@users.noreply.github.com>
2024-01-26 20:20:56 +0100

    added login logic

--- src/studentmangementapp/FXMLDocumentController.java
+++ src/studentmangementapp/FXMLDocumentController.java
@@ -12,13 +12,17 @@ import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -65,9 +69,28 @@ public class FXMLDocumentController implements Initializable {
                 alert.showAndWait();
             } else {
                 if (result.next()) {
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully logged in!");
+                    alert.showAndWait();
+
+                    loginBtn.getScene().getWindow().hide();
+
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setScene(scene);
+                    stage.show();
 
                 } else {
-
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
                 }
             }
         } catch (Exception e) {
